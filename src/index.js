9cb178ca04243972aa614ee814ded0837fde61a9
hnakae <nakae27@gmail.com>
2022-06-14 06:48:12 -0700

    react18 createRoot react-dom/client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 
 import App from './App.js';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
