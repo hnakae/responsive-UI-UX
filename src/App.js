523bb38dd044042a420a29758e84eaa68aa40f05
hnakae <nakae27@gmail.com>
2022-06-14 03:57:43 -0700

    app.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 
-import { Blog, Features, Footer, Header, Possiblity, WhatGPT3 } from './containers';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
